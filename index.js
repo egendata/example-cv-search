73fe642dda00489a5297e33346880161ba0d385d
Christian Landgren <christian@landgren.nu>
2020-01-10 11:43:07 +0100

    added server

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './src/app'
-const state = {}
+
+const state = {
+  firstName: 'John',
+  lastName: 'Doe'
+}
 
 ReactDOM.render(<App {...state}></App>, document.getElementById('root'))
--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default function App ({firstName, lastName}) {
-  return <div><h1>CV Search</h1></div>
+  return <div><h1>Hi {firstName} {lastName}!</h1></div>
 }
