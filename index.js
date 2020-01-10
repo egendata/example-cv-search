408a4ab12167003bb86fb26ce635c60d5a38e8e2
Christian Landgren <christian@landgren.nu>
2020-01-10 11:15:16 +0100

    new react boilerplate

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './src/app'
+const state = {}
+
+ReactDOM.render(<App {...state}></App>, document.getElementById('root'))
---
+++ src/app.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function App ({firstName, lastName}) {
+  return <div><h1>CV Search</h1></div>
+}
