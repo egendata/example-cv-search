bdf7f6e2def85e81ca5d0d66eeaab96afd06ad99
Christian Landgren <christian@landgren.nu>
2020-01-15 14:25:16 +0100

    Use default config

--- server/operatorClient.js
+++ server/operatorClient.js
@@ -3,12 +3,13 @@ const { createMemoryStore } = require('@egendata/client/lib/memoryStore')
 const { readFileSync } = require('fs')
 
 const clientKey = process.env.PRIVATE_KEY || readFileSync('./dev_key.pem', 'utf8')
+const clientId = process.env.CLIENT_ID || 'http://localhost:5000'
 
 const config = {
   displayName: 'CV Search',
   description: 'Indexerar ditt CV',
-  iconURI: `${process.env.CLIENT_ID}/android-icon-96x96.png`,
-  clientId: process.env.CLIENT_ID || 'http://localhost:5000',
+  iconURI: `${clientId}/android-icon-96x96.png`,
+  clientId: clientId || 'http://localhost:5000',
   operator: process.env.OPERATOR_URL || 'http://localhost:3000',
   jwksPath: '/jwks',
   eventsPath: '/events',
